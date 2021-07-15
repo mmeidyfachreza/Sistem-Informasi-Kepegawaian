562b0ba751a2ab49ab40269af5da0697b9c504c5
Mei.dy <meidyfachreza@gmail.com>
2021-07-15 14:47:11 +0800

    add logo

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -76,7 +76,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-      <img src="{{asset('assets/dist/img/bontang.png')}}" alt="logo" class="brand-image" style="max-height:44px">
+      <img src="{{asset('assets/dist/img/logo.png')}}" alt="logo" class="brand-image" style="max-height:44px">
       <span class="brand-text font-weight-light">SI Kepegawaian</span>
     </a>
 
